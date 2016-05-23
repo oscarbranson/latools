39562ebdaa0f5c52cea0c053d462c18487bae343
Oscar Branson <oscarbranson@gmail.com>
2016-05-23 10:45:00 -0700

    Version changed to 0.2 to reflect filtering changes.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='latools',
-      version='0.1',
+      version='0.2',
       description='Tools for LA-ICPMS data analysis.',
       url='https://github.com/oscarbranson/latools',
       author='Oscar Branson',
