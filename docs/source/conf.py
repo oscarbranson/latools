9755c1e2a72e2d726d590c36aef58de3dd2e433f
Oscar Branson <oscarbranson@gmail.com>
2016-06-09 08:59:18 -0700

    Changed documentation theme to rtd.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,6 +38,8 @@ extensions = [
     'numpydoc',
 ]
 
+numpydoc_show_class_members = False
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
@@ -116,7 +118,7 @@ todo_include_todos = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'nature'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
