c6eb87a60235696d6c5a138b61ba073de107311e
Oscar <oscarbranson@gmail.com>
2017-11-10 16:25:19 +1100

    improved behaviour: threshold method fallback, fornat warnings, catch empty, median_scaler, threshold_mult option

--- latools/signal_optimiser.py
+++ latools/signal_optimiser.py
@@ -6,8 +6,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import bayes_mvs
 from scipy.stats.kde import gaussian_kde
-from .helpers import Bunch, rolling_window, nominal_values
+from .helpers import Bunch, rolling_window, nominal_values, bool_2_indices, _warning
 from .plots import tplot
+
+warnings.showwarning = _warning
 
 def calc_windows(fn, s, min_points):
     """
@@ -28,7 +30,7 @@ def calc_windows(fn, s, min_points):
 
     return out
 
-def calc_window_mean_std(s, min_points):
+def calc_window_mean_std(s, min_points, ind=None):
     """
     Apply fn to all contiguous regions in s that have at least min_points.
     """
@@ -39,7 +41,10 @@ def calc_window_mean_std(s, min_points):
     std = np.full((n_points, s.size), np.nan)
 
     # skip nans, for speed
-    ind = ~np.isnan(s)
+    if ind is None:
+        ind = ~np.isnan(s)
+    else:
+        ind = ind & ~np.isnan(s)
     s = s[ind]
 
     for i, w in enumerate(range(min_points, s.size)):
@@ -65,9 +70,25 @@ def bayes_scale(s):
     else:
         return np.full(s.shape, np.nan)
 
+def median_scaler(s):
+    """
+    Remove median, divide by IQR.
+    """
+    if sum(~np.isnan(s)) > 2:
+        ss = s[~np.isnan(s)]
+        median = np.median(ss)
+        IQR = np.diff(np.percentile(ss, [25, 75]))
+        return (s - median) / IQR
+    else:
+        return np.full(s.shape, np.nan)
+
+# scaler = bayes_scale
+scaler = median_scaler
+
 def signal_optimiser(d, analytes, min_points=5,
                      threshold_mode='kde_first_max',
-                     weights=None):
+                     threshold_mult=1.,
+                     weights=None, ind=None):
     """
     Optimise data selection based on specified analytes.
 
@@ -110,11 +131,20 @@ def signal_optimiser(d, analytes, min_points=5,
         or 'bayes_mvs', or a custom function. If a
         function, must take a 1D array, and return a
         single, real number.
+    threshood_mult : float or tuple
+        A multiplier applied to the calculated threshold
+        before use. If a tuple, the first value is applied
+        to the mean threshold, and the second is applied to
+        the standard deviation threshold. Reduce this to make
+        data selection more stringent.
     weights : array-like of length len(analytes)
         An array of numbers specifying the importance of
         each analyte considered. Larger number makes the
         analyte have a greater effect on the optimisation.
         Default is None.
+    ind : boolean array
+        A boolean array the same length as the data. Where
+        false, data will not be included.
 
     Returns
     -------
@@ -123,26 +153,41 @@ def signal_optimiser(d, analytes, min_points=5,
     if isinstance(analytes, str):
         analytes = [analytes]
 
+    if not any(ind) or (np.diff(bool_2_indices(ind)).max() < min_points):
+        warnings.warn('Optmisation failed on {:}. No contiguous data regions longer than {:.0f} points.'.format(d.sample, min_points))
+        return Bunch({'means': np.nan,
+                      'stds': np.nan,
+                      'mean_threshold': np.nan,
+                      'std_threshold': np.nan,
+                      'lims': np.nan,
+                      'filt': ind,
+                      'threshold_mode': threshold_mode,
+                      'min_points': min_points,
+                      'analytes': analytes,
+                      'opt_centre': np.nan,
+                      'opt_n_points': np.nan,
+                      'weights': weights,
+                      'optimisation_success': False})
+
     # calculate statistics
     stds = []
     means = []
     with warnings.catch_warnings():
         warnings.simplefilter("ignore")
         for a in analytes:
-            m, s = calc_window_mean_std(nominal_values(d.focus[a]), min_points)
-            # stds.append(calc_windows(np.nanstd, nominal_values(d.focus[a]), min_points))
-            # means.append(calc_windows(np.nanmean, nominal_values(d.focus[a]), min_points))
+            m, s = calc_window_mean_std(nominal_values(d.focus[a]), min_points, ind)
             means.append(m)
             stds.append(s)
-        # compile stats
+    # compile stats
     stds = np.array(stds)
     means = np.array(means)
 
-    # sstds = stds / means
+    # calculate rsd
+    sstds = stds / abs(means)
 
-    # scale means and standard deviations
-    smeans = np.apply_along_axis(bayes_scale, 1, means)
-    sstds = np.apply_along_axis(bayes_scale, 1, stds)
+    # scale means for each analyte
+    smeans = np.apply_along_axis(scaler, 2, means)
+    # sstds = np.apply_along_axis(scaler, 2, stds)
 
     # apply weights
     if weights is not None:
@@ -154,78 +199,100 @@ def signal_optimiser(d, analytes, min_points=5,
     msmeans = smeans.mean(0)
 
     # define thresholds
-    valid = ['median', 'mean', 'kde_max', 'kde_first_max', 'bayes_mvs']
-    if threshold_mode == 'median':
-        # median - OK, but best?
-        std_threshold = np.nanmedian(msstds)
-        mean_threshold = np.nanmedian(msmeans)
-    elif threshold_mode == 'mean':
-        # mean
-        std_threshold = np.nanmean(msstds)
-        mean_threshold = np.nanmean(msmeans)
-    elif threshold_mode == 'kde_max':
-        # maximum of gaussian kernel density estimator
-        mkd = gaussian_kde(msmeans[~np.isnan(msmeans)])
-        xm = np.linspace(*np.percentile(msmeans.flatten()[~np.isnan(msmeans.flatten())], (1, 99)), 100)
-        mdf = mkd.pdf(xm)
-        mean_threshold = xm[np.argmax(mdf)]
+    valid = ['kde_first_max', 'kde_max', 'median', 'bayes_mvs', 'mean']
+    n_under = 0
+    i = np.argwhere(np.array(valid) == threshold_mode)[0, 0]
+    o_threshold_mode = threshold_mode
+    while (n_under <= 0) & (i < len(valid)):
+        if threshold_mode == 'median':
+            # median - OK, but best?
+            std_threshold = np.nanmedian(msstds)
+            mean_threshold = np.nanmedian(msmeans)
+        elif threshold_mode == 'mean':
+            # mean
+            std_threshold = np.nanmean(msstds)
+            mean_threshold = np.nanmean(msmeans)
+        elif threshold_mode == 'kde_max':
+            # maximum of gaussian kernel density estimator
+            mkd = gaussian_kde(msmeans[~np.isnan(msmeans)])
+            xm = np.linspace(*np.percentile(msmeans.flatten()[~np.isnan(msmeans.flatten())], (1, 99)), 100)
+            mdf = mkd.pdf(xm)
+            mean_threshold = xm[np.argmax(mdf)]
 
-        rkd = gaussian_kde(msstds[~np.isnan(msstds)])
-        xr = np.linspace(*np.percentile(msstds.flatten()[~np.isnan(msstds.flatten())], (1, 99)), 100)
-        rdf = rkd.pdf(xr)
-        std_threshold = xr[np.argmax(rdf)]
-    elif threshold_mode == 'kde_first_max':
-        # first local maximum of gaussian kernel density estimator
-        mkd = gaussian_kde(msmeans[~np.isnan(msmeans)])
-        xm = np.linspace(*np.percentile(msmeans.flatten()[~np.isnan(msmeans.flatten())], (1, 99)), 100)
-        mdf = mkd.pdf(xm)
-        inds = np.argwhere(np.r_[False, mdf[1:] > mdf[:-1]] & 
-                           np.r_[mdf[:-1] > mdf[1:], False] & 
-                           (mdf > 0.25 * mdf.max()))
-        mean_threshold = xm[np.min(inds)]
+            rkd = gaussian_kde(msstds[~np.isnan(msstds)])
+            xr = np.linspace(*np.percentile(msstds.flatten()[~np.isnan(msstds.flatten())], (1, 99)), 100)
+            rdf = rkd.pdf(xr)
+            std_threshold = xr[np.argmax(rdf)]
+        elif threshold_mode == 'kde_first_max':
+            # first local maximum of gaussian kernel density estimator
+            mkd = gaussian_kde(msmeans[~np.isnan(msmeans)])
+            xm = np.linspace(*np.percentile(msmeans.flatten()[~np.isnan(msmeans.flatten())], (1, 99)), 100)
+            mdf = mkd.pdf(xm)
+            inds = np.argwhere(np.r_[False, mdf[1:] > mdf[:-1]] & 
+                            np.r_[mdf[:-1] > mdf[1:], False] & 
+                            (mdf > 0.25 * mdf.max()))
+            mean_threshold = xm[np.min(inds)]
 
-        rkd = gaussian_kde(msstds[~np.isnan(msstds)])
-        xr = np.linspace(*np.percentile(msstds.flatten()[~np.isnan(msstds.flatten())], (1, 99)), 100)
-        rdf = rkd.pdf(xr)
-        inds = np.argwhere(np.r_[False, rdf[1:] > rdf[:-1]] & 
-                           np.r_[rdf[:-1] > rdf[1:], False] & 
-                           (rdf > 0.25 * rdf.max()))
-        std_threshold = xr[np.min(inds)]
-    elif threshold_mode == 'bayes_mvs':
-        # bayesian mvs.
-        bm, _, bs = bayes_mvs(msstds[~np.isnan(msstds)])
-        std_threshold = bm.statistic
+            rkd = gaussian_kde(msstds[~np.isnan(msstds)])
+            xr = np.linspace(*np.percentile(msstds.flatten()[~np.isnan(msstds.flatten())], (1, 99)), 100)
+            rdf = rkd.pdf(xr)
+            inds = np.argwhere(np.r_[False, rdf[1:] > rdf[:-1]] & 
+                            np.r_[rdf[:-1] > rdf[1:], False] & 
+                            (rdf > 0.25 * rdf.max()))
+            std_threshold = xr[np.min(inds)]
+        elif threshold_mode == 'bayes_mvs':
+            # bayesian mvs.
+            bm, _, bs = bayes_mvs(msstds[~np.isnan(msstds)])
+            std_threshold = bm.statistic
 
-        bm, _, bs = bayes_mvs(msmeans[~np.isnan(msmeans)])
-        mean_threshold = bm.statistic
-    elif callable(threshold_mode):
-        std_threshold = threshold_mode(msstds[~np.isnan(msstds)].flatten())
-        mean_threshold = threshold_mode(msmeans[~np.isnan(msmeans)].flatten())
-    else:
-        try:
-            mean_threshold, std_threshold = threshold_mode
-        except:
-            raise ValueError('\nthreshold_mode must be one of:\n   ' + ', '.join(valid) + ',\na custom function, or a \n(mean_threshold, std_threshold) tuple.')
+            bm, _, bs = bayes_mvs(msmeans[~np.isnan(msmeans)])
+            mean_threshold = bm.statistic
+        elif callable(threshold_mode):
+            std_threshold = threshold_mode(msstds[~np.isnan(msstds)].flatten())
+            mean_threshold = threshold_mode(msmeans[~np.isnan(msmeans)].flatten())
+        else:
+            try:
+                mean_threshold, std_threshold = threshold_mode
+            except:
+                raise ValueError('\nthreshold_mode must be one of:\n   ' + ', '.join(valid) + ',\na custom function, or a \n(mean_threshold, std_threshold) tuple.')
+
+        # apply threshold_mult
+        if isinstance(threshold_mult, (int, float)):
+            std_threshold *= threshold_mult
+            mean_threshold *= threshold_mult
+        elif len(threshold_mult) == 2:
+            mean_threshold *= threshold_mult[0]
+            std_threshold *= threshold_mult[1]
+        else:
+            raise ValueError('\nthreshold_mult must be a float, int or tuple of length 2.')
+
+        rind = (msstds < std_threshold)
+        mind = (msmeans < mean_threshold)
+
+        ind = rind & mind
+
+        n_under = ind.sum()
+        if n_under <= 0:
+            i += 1
+            threshold_mode = valid[i]
+    
+    if i > 0:
+        warnings.warn("Optimisation failed on {:} using threshold_mode='{:}', falling back to '{:}'".format(d.sample, o_threshold_mode, threshold_mode))
 
     # identify max number of points within thresholds
     centres, npoints = np.meshgrid(np.arange(msmeans.shape[1]),
                                    np.arange(min_points, min_points + msmeans.shape[0]))
-    rind = (msstds < std_threshold - 0.01)
-    mind = (msmeans < mean_threshold - 0.01)
-
-    ind = rind & mind
     opt_n_points = npoints[ind].max()
     # plus/minus one point to allow some freedom to shift selection window.
-    cind = ind & ((npoints <= opt_n_points + 1) | (npoints >= opt_n_points - 1))
+    cind = ind & (npoints == opt_n_points)
     opt_centre = centres[cind].min()
-    # opt_centre = opt_centres[msmeans[cind] == np.nanmin(msmeans[cind])][0]
 
     if opt_n_points % 2 == 0:
         lims = (opt_centre - opt_n_points // 2,
-                opt_centre + opt_n_points // 2 + 1)
+                opt_centre + opt_n_points // 2)
     else:
-        lims = (opt_centre - opt_n_points // 2 - 1,
-                opt_centre + opt_n_points // 2 - 1)
+        lims = (opt_centre - opt_n_points // 2,
+                opt_centre + opt_n_points // 2 + 1)
 
     filt = np.zeros(d.Time.shape, dtype=bool)
     filt[lims[0]:lims[1]] = True
@@ -241,7 +308,8 @@ def signal_optimiser(d, analytes, min_points=5,
                   'analytes': analytes,
                   'opt_centre': opt_centre,
                   'opt_n_points': opt_n_points,
-                  'weights': weights})
+                  'weights': weights,
+                  'optimisation_success': True})
 
 
 def optimisation_plot(d, overlay_alpha=0.5, **kwargs):
@@ -262,6 +330,10 @@ def optimisation_plot(d, overlay_alpha=0.5, **kwargs):
     """
     if not hasattr(d, 'opt'):
         raise ValueError('Please run `signal_optimiser` before trying to plot its results.')
+    
+    if not d.opt['optimisation_success']:
+        warnings.warn('Cannot plot {:}. No optimisation result.'.format(d.sample))
+        return None, None
 
     # unpack variables
     means = d.opt['means']
@@ -276,6 +348,10 @@ def optimisation_plot(d, overlay_alpha=0.5, **kwargs):
     rind = (stds < std_threshold)
     mind = (means < mean_threshold)
 
+    # color scale and histogram limits
+    mlim = np.percentile(means.flatten()[~np.isnan(means.flatten())], (0, 99))
+    rlim = np.percentile(stds.flatten()[~np.isnan(stds.flatten())], (0, 99))
+
     cmr = plt.cm.Blues
     cmr.set_bad((0,0,0,0.3))
 
@@ -288,14 +364,14 @@ def optimisation_plot(d, overlay_alpha=0.5, **kwargs):
     ma = fig.add_subplot(3, 2, 1)
     ra = fig.add_subplot(3, 2, 2)
 
-    mm = ma.imshow(means, origin='bottomleft', cmap=cmm,
+    mm = ma.imshow(means, origin='bottomleft', cmap=cmm, vmin=mlim[0], vmax=mlim[1],
                    extent=(centres.min(), centres.max(), npoints.min(), npoints.max()))
 
     ma.set_ylabel('N points')
     ma.set_xlabel('Center')
     fig.colorbar(mm, ax=ma, label='Amplitude')
 
-    mr = ra.imshow(stds, origin='bottomleft', cmap=cmr,
+    mr = ra.imshow(stds, origin='bottomleft', cmap=cmr, vmin=rlim[0], vmax=rlim[1],
                    extent=(centres.min(), centres.max(), npoints.min(), npoints.max()))
 
     # ra.set_ylabel('N points')
@@ -315,7 +391,7 @@ def optimisation_plot(d, overlay_alpha=0.5, **kwargs):
     mah = fig.add_subplot(3, 2, 3)
     rah = fig.add_subplot(3, 2, 4)
 
-    mah.set_xlim(*np.percentile(means.flatten()[~np.isnan(means.flatten())], (0, 99)))
+    mah.set_xlim(mlim)
     mbin = np.linspace(*mah.get_xlim(), 50)
     mah.hist(means.flatten()[~np.isnan(means.flatten())], mbin)
     mah.axvspan(mean_threshold, mah.get_xlim()[1], color=(0,0,0,overlay_alpha))
@@ -324,7 +400,7 @@ def optimisation_plot(d, overlay_alpha=0.5, **kwargs):
     mah.set_xlabel('Scaled Mean Analyte Conc')
     mah.set_ylabel('N')
 
-    rah.set_xlim(np.percentile(stds.flatten()[~np.isnan(stds.flatten())], (0, 99)))
+    rah.set_xlim(rlim)
     rbin = np.linspace(*rah.get_xlim(), 50)
     rah.hist(stds.flatten()[~np.isnan(stds.flatten())], rbin)
     rah.axvspan(std_threshold, rah.get_xlim()[1], color=(0,0,0,0.4))
