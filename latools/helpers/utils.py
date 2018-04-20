640d49c87f637285757c0ef48251c8073454f992
Oscar <oscarbranson@gmail.com>
2018-04-20 15:13:19 +1000

    catch missing srm_table

--- latools/helpers/utils.py
+++ latools/helpers/utils.py
@@ -102,6 +102,7 @@ def read_logfile(log_file):
             runargs[fname]['kwargs']['config'] = 'REPRODUCE'
             runargs[fname]['kwargs']['dataformat'] = None
             runargs[fname]['kwargs']['data_folder'] = paths['data_folder']
-            runargs[fname]['kwargs']['srm_file'] = paths['srm_table']
+            if 'srm_table' in paths:
+                runargs[fname]['kwargs']['srm_file'] = paths['srm_table']
 
     return runargs, paths
