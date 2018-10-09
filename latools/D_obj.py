1f979b115d139077228c258ae75a426c4b8e8c5b
Oscar <oscarbranson@gmail.com>
2018-10-09 14:50:14 +1100

    spectral lag correction

--- latools/D_obj.py
+++ latools/D_obj.py
@@ -420,6 +420,46 @@ class D(object):
         return
 
     @_log
+    def correct_spectral_interference(self, target_analyte, source_analyte, f):
+        """
+        Correct spectral interference.
+
+        Subtract interference counts from target_analyte, based on the
+        intensity of a source_analayte and a known fractional contribution (f).
+
+        Correction takes the form:
+        target_analyte -= source_analyte * f
+
+        Only operates on background-corrected data ('bkgsub'). 
+        
+        To undo a correction,
+        rerun `self.bkg_subtract()`.
+
+        Parameters
+        ----------
+        target_analyte : str
+            The name of the analyte to modify.
+        source_analyte : str
+            The name of the analyte to base the correction on.
+        f : float
+            The fraction of the intensity of the source_analyte to
+            subtract from the target_analyte. Correction is:
+            target_analyte - source_analyte * f
+
+        Returns
+        -------
+        None
+        """
+
+        if target_analyte not in self.analytes:
+            raise ValueError('target_analyte: {:} not in available analytes ({:})'.format(target_analyte, ', '.join(self.analytes)))
+
+        if source_analyte not in self.analytes:
+            raise ValueError('source_analyte: {:} not in available analytes ({:})'.format(source_analyte, ', '.join(self.analytes)))
+
+        self.data['bkgsub'][target_analyte] -= self.data['bkgsub'][source_analyte] * f
+
+    @_log
     def ratio(self, internal_standard=None):
         """
         Divide all analytes by a specified internal_standard analyte.
@@ -785,7 +825,7 @@ class D(object):
                 ds = preprocessing.scale(ds)
 
             method_key = {'kmeans': clustering.cluster_kmeans,
-                          'DBSCAN': clustering.cluster_DBSCAN,
+                        #   'DBSCAN': clustering.cluster_DBSCAN,
                           'meanshift': clustering.cluster_meanshift}
 
             cfun = method_key[method]
--- latools/latools.py
+++ latools/latools.py
@@ -1018,6 +1018,57 @@ class analyse(object):
         return
 
     @_log
+    def correct_spectral_interference(self, target_analyte, source_analyte, f):
+        """
+        Correct spectral interference.
+
+        Subtract interference counts from target_analyte, based on the
+        intensity of a source_analayte and a known fractional contribution (f).
+
+        Correction takes the form:
+        target_analyte -= source_analyte * f
+
+        Only operates on background-corrected data ('bkgsub'). To undo a correction,
+        rerun `self.bkg_subtract()`.
+
+        Example
+        -------
+        To correct 44Ca+ for an 88Sr++ interference, where both 43.5 and 44 Da
+        peaks are known:
+        f = abundance(88Sr) / (abundance(87Sr) 
+
+        counts(44Ca) = counts(44 Da) - counts(43.5 Da) * f
+
+
+        Parameters
+        ----------
+        target_analyte : str
+            The name of the analyte to modify.
+        source_analyte : str
+            The name of the analyte to base the correction on.
+        f : float
+            The fraction of the intensity of the source_analyte to
+            subtract from the target_analyte. Correction is:
+            target_analyte - source_analyte * f
+
+        Returns
+        -------
+        None
+        """
+
+        if target_analyte not in self.analytes:
+            raise ValueError('target_analyte: {:} not in available analytes ({:})'.format(target_analyte, ', '.join(self.analytes)))
+
+        if source_analyte not in self.analytes:
+            raise ValueError('source_analyte: {:} not in available analytes ({:})'.format(source_analyte, ', '.join(self.analytes)))
+
+        with self.pbar.set(total=len(self.data), desc='Interference Correction') as prog:
+            for d in self.data.values():
+                d.correct_spectral_interference(target_analyte, source_analyte, f)
+
+                prog.update()
+
+    @_log
     def bkg_plot(self, analytes=None, figsize=None, yscale='log',
                  ylim=None, err='stderr', save=True):
         """
