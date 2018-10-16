344df9f1f6c51146dc3849a97e36b7703ad35ca7
Oscar <oscarbranson@gmail.com>
2018-10-16 19:45:07 +1100

    version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='latools',
-      version='0.3.4',
+      version='0.3.5',
       description='Tools for LA-ICPMS data analysis.',
       url='https://github.com/oscarbranson/latools',
       author='Oscar Branson',
