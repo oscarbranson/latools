bd35d573e01a4bddbda052f60743e5bea3939b3a
Oscar <oscarbranson@gmail.com>
2018-12-23 21:02:38 +1000

    nominal_values and std_devs import at top level

--- latools/__init__.py
+++ latools/__init__.py
@@ -1,6 +1,7 @@
 from .latools import analyse, reproduce
 from .latools import analyse as analyze  # for the yanks
-from .helpers.helpers import get_example_data, nominal_values
+from .helpers.helpers import get_example_data
+from .helpers.stat_fns import nominal_values, std_devs
 from .helpers import config
 
 # from . import pca
