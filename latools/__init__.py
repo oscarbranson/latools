fdea0855e4354dfee73b904458fb85abaa1aba5d
Oscar <oscarbranson@gmail.com>
2019-06-06 16:43:26 +1000

    Closes #36

--- latools/__init__.py
+++ latools/__init__.py
@@ -3,6 +3,7 @@ from .latools import analyse as analyze  # for the yanks
 from .helpers.helpers import get_example_data
 from .helpers.stat_fns import nominal_values, std_devs
 from .helpers import config
+from . import preprocessing
 
 # from . import pca
 # from . import helpers
