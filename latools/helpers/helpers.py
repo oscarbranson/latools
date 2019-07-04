f0cebb38cbcb498341574237c856eb2e1645896e
Oscar <oscarbranson@gmail.com>
2019-07-04 16:00:26 +1000

    Revert "updated interp1d function to avoid use of extrapolate outside"
    This reverts commit 048a14c7dac02290d8db743b605a26e9026c8683.

--- latools/helpers/helpers.py
+++ latools/helpers/helpers.py
@@ -313,18 +313,11 @@ class un_interp1d(object):
     object for handling interpolation of values with uncertainties.
     """
 
-    def __init__(self, x, y, fill_value=np.nan, **kwargs):
-        if isinstance(fill_value, tuple):
-            nom_fill = tuple([un.nominal_values(v) for v in fill_value])
-            std_fill = tuple([un.std_devs(v) for v in fill_value])
-        else:
-            nom_fill = std_fill = fill_value
+    def __init__(self, x, y, **kwargs):
         self.nom_interp = interp.interp1d(un.nominal_values(x),
-                                          un.nominal_values(y), 
-                                          fill_value=nom_fill, **kwargs)
+                                          un.nominal_values(y), **kwargs)
         self.std_interp = interp.interp1d(un.nominal_values(x),
-                                          un.std_devs(y), 
-                                          fill_value=std_fill, **kwargs)
+                                          un.std_devs(y), **kwargs)
 
     def new(self, xn):
         yn = self.nom_interp(xn)
