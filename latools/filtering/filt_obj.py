32f31417035249874bec9b1c594200eb9aef5f65
Oscar Branson <ob266@cam.ac.uk>
2021-04-06 23:36:16 +0100

    docstrings for new filt object

--- latools/filtering/filt_obj.py
+++ latools/filtering/filt_obj.py
@@ -9,6 +9,46 @@ from difflib import SequenceMatcher as seqm
 from latools.helpers.helpers import bool_2_indices, Bunch
 
 class filt(object):
+    """
+    Container for creating, storing and selecting data filters.
+
+    Parameters
+    ----------
+    size : int
+        The length that the filters need to be (should be
+        the same as your data).
+    analytes : array_like
+        A list of the analytes measured in your data.
+
+    Attributes
+    ----------
+    size : int
+        The length that the filters need to be (should be
+        the same as your data).
+    analytes : array_like
+        A list of the analytes measured in your data.
+    components : dict
+        A dict containing each individual filter that has been
+        created.
+    info : dict
+        A dict containing descriptive information about each
+        filter in `components`.
+    params : dict
+        A dict containing the parameters used to create
+        each filter, which can be passed directly to the
+        corresponding filter function to recreate the filter.
+    switches : dict
+        A dict of boolean switches specifying which filters
+        are active for each analyte.
+    keys : dict
+        A dict of logical strings specifying which filters are
+        applied to each analyte.
+    sequence : dict
+        A numbered dict specifying what order the filters were
+        applied in (for some filters, order matters).
+    n : int
+        The number of filters applied to the data.
+    """
     
     def __init__(self, size, analytes):
         self.size = size
@@ -27,6 +67,26 @@ class filt(object):
         self.N = 0
 
     def add(self, name, filt, info='', params=(), setn=None):
+        """
+        Add filter.
+
+        Parameters
+        ----------
+        name : str
+            filter name
+        filt : array_like
+            boolean filter array
+        info : str
+            informative description of the filter
+        params : tuple
+            parameters used to make the filter
+        setn : int
+            the set number of the filter
+
+        Returns
+        -------
+        None
+        """
         
         if setn is None:
             setn = self.maxset + 1
@@ -42,15 +102,48 @@ class filt(object):
         self.fnames.append(f'{setn}:{name}')
             
     def remove(self, name=None, setn=None):
-        pass
+        """
+        Remove filter.
+
+        Parameters
+        ----------
+        name : str
+            name of the filter to remove
+        setn : int or True
+            int: number of set to remove
+            True: remove all filters in set that 'name' belongs to
+
+        Returns
+        -------
+        None
+        """
+        raise DeprecationWarning('This no longer works. Use `.filter_clear()` instead, then re-run the filters you want to keep.')
 
     def clear(self):
+        """
+        Clear all filters.
+        """
         self.__init__(self.size, self.analytes)
     
     def clean(self):
-        pass
+        raise DeprecationWarning('This no longer works.')
     
     def on(self, analyte=None, filt=None):
+        """
+        Turn on specified filter(s) for specified analyte(s).
+
+        Parameters
+        ----------
+        analyte : optional, str or array_like
+            Name or list of names of analytes.
+            Defaults to all analytes.
+        filt : optional. int, str or array_like
+            Name/number or iterable names/numbers of filters.
+
+        Returns
+        -------
+        None
+        """
         if isinstance(analyte, str):
             analyte = [analyte]
         if analyte is None:
@@ -63,6 +156,21 @@ class filt(object):
         self.filter_table.loc[(n, filt), analyte] = True
     
     def off(self, analyte=None, filt=None):
+        """
+        Turn off specified filter(s) for specified analyte(s).
+
+        Parameters
+        ----------
+        analyte : optional, str or array_like
+            Name or list of names of analytes.
+            Defaults to all analytes.
+        filt : optional. int, list of int or str
+            Number(s) or partial string that corresponds to filter name(s).
+
+        Returns
+        -------
+        None
+        """
         if isinstance(analyte, str):
             analyte = [analyte]
         if analyte is None:
@@ -75,6 +183,20 @@ class filt(object):
         self.filter_table.loc[(n, filt), analyte] = False
     
     def fuzzmatch(self, fuzzkey, multi=True):
+        """
+        Identify a filter by fuzzy string matching.
+
+        Partial ('fuzzy') matching performed by `fuzzywuzzy.fuzzy.ratio`
+
+        Parameters
+        ----------
+        fuzzkey : str
+            A string that partially matches one filter name more than the others.
+
+        Returns
+        -------
+        The name of the most closely matched filter. : str
+        """
         keys, ratios = np.array([(f, seqm(None, fuzzkey, f).ratio()) for f in self.fnames]).T
         mratio = max(ratios)
         
@@ -90,6 +212,21 @@ class filt(object):
         return int(n), filt
     
     def make_analyte(self, analyte):
+        """
+        Make filter for specified analyte(s).
+
+        Filter specified in filt.switches.
+
+        Parameters
+        ----------
+        analyte : str or array_like
+            Name or list of names of analytes.
+
+        Returns
+        -------
+        array_like
+            boolean filter
+        """
         if isinstance(analyte, str):
             analyte = [analyte]
         elif analyte is None:
@@ -102,6 +239,29 @@ class filt(object):
         return self.make_fromkey('&'.join(key))
     
     def make_fromkey(self, key):
+        """
+        Make filter from logical expression.
+
+        Takes a logical expression as an input, and returns a filter. Used for advanced
+        filtering, where combinations of nested and/or filters are desired. Filter names must
+        exactly match the names listed by print(filt).
+
+        Example: ``key = '(Filter_1 | Filter_2) & Filter_3'``
+        is equivalent to:
+        ``(Filter_1 OR Filter_2) AND Filter_3``
+        statements in parentheses are evaluated first.
+
+        Parameters
+        ----------
+        key : str
+            logical expression describing filter construction.
+
+        Returns
+        -------
+        array_like
+            boolean filter
+
+        """
         if key != '':
             def make_runable(match):
                 return "self.filter_components.loc[:," + str(tuple(self.fuzzmatch(match.group(0)))) + "]"
@@ -111,6 +271,20 @@ class filt(object):
             return ~np.zeros(self.size, dtype=bool)
         
     def make_keydict(self, analyte=None):
+        """
+        Make logical expressions describing the filter(s) for specified analyte(s).
+
+        Parameters
+        ----------
+        analyte : optional, str or array_like
+            Name or list of names of analytes.
+            Defaults to all analytes.
+
+        Returns
+        -------
+        dict
+            containing the logical filter expression for each analyte.
+        """
         if isinstance(analyte, str):
             analyte = [analyte]
         elif analyte is None:
@@ -123,6 +297,22 @@ class filt(object):
             self.keydict[a] = ' & '.join(key)
     
     def grab_filt(self, filt, analyte=None):
+        """
+        Flexible access to specific filter using any key format.
+
+        Parameters
+        ----------
+        f : str, dict or bool
+            either logical filter expression, dict of expressions,
+            or a boolean
+        analyte : str
+            name of analyte the filter is for.
+
+        Returns
+        -------
+        array_like
+            boolean filter
+        """
         if isinstance(filt, str):
             if filt in self.fnames:
                 fkey = self.fuzzmatch(filt)
@@ -151,9 +341,12 @@ class filt(object):
         return ind
     
     def get_components(self, analyte):
-        pass
+        raise DeprecationWarning('This no longer works.')
     
     def get_info(self):
+        """
+        Get info for all filters.
+        """
         out = ''
         for k in sorted(self.info.keys()):
             out += f'{k}: {self.info[k]}\n'
@@ -638,71 +831,3 @@ class filt(object):
 #             out += '{:s}: {:s}'.format(k, self.info[k]) + '\n'
 #         return(out)
 
-
-
-## TODO: [Low Priority] Re-write filt object to use pandas?
-
-# class filt(object):
-    
-#     def __init__(self, size, analytes):
-#         self.size = size
-#         self.analytes = analytes
-        
-#         self.filter_table = pd.DataFrame(index=pd.MultiIndex(levels=[[], []], labels=[[], []], names=['N', 'desc']),
-#                                          columns=self.analytes)
-        
-#         self.filters = Bunch()
-#         self.param = Bunch()
-#         self.info = Bunch()
-        
-#         self.N = 0
-
-#     def __repr__(self):
-#         pass
-
-#     def add(self, name, filt, info='', params=()):
-        
-#         self.filters[self.N] = filt
-#         self.param[self.N] = params
-#         self.info[self.N] = info
-        
-#         self.filter_table.loc[(self.N, name), :] = False
-        
-#         self.N += 1
-    
-#     def remove(self):
-#         pass
-
-#     def clear(self):
-#         self.__init__(self.size, self.analytes)
-    
-#     def clean(self):
-#         pass
-    
-#     def on(self):
-#         pass
-    
-#     def off(self):
-#         pass
-    
-#     def make(self):
-#         pass
-    
-#     def fuzzmatch(self):
-#         pass
-    
-#     def make_fromkey(self):
-#         pass
-    
-#     def make_keydict(self):
-#         pass
-    
-#     def grab_filt(self):
-#         pass
-    
-#     def get_components(self):
-#         pass
-    
-#     def get_info(self):
-#         pass
-    
