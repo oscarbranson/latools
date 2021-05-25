fix mass fraction calculation to work with analyte ratios

latools/D_obj.py
@@ -578,8 +578,6 @@ class D(object):
         return
 
     def calc_mass_fraction(self, internal_standard_conc, analytes=None, analyte_masses=None):
-        if analytes is None:
-            analytes = self.analytes.difference(self.internal_standard)
         if 'mass_fraction' not in self.data.keys():
             self.data['mass_fraction'] = Bunch()
 
@@ -595,7 +593,10 @@ class D(object):
                 num, denom = a.split('_')
                 self.data['mass_fraction'][num] = to_mass_fraction(molar_ratio=self.data['calibrated'][a], massfrac_denominator=internal_standard_conc,
                                                                    numerator_mass=analyte_masses[num], denominator_mass=analyte_masses[denom])
-        
+                if denom not in self.data['mass_fraction']:
+                    self.data['mass_fraction'][denom] = np.full(self.data['calibrated'][a].shape, np.nan)
+                    self.data['mass_fraction'][denom][~np.isnan(un.nominal_values(self.data['mass_fraction'][num]))] = internal_standard_conc
+
         self.setfocus('mass_fraction')
 
 