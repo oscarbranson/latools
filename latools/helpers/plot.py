e14153060e1d6f50793462e9f6fa8290ebd1356e
Oscar Branson <oscarbranson@gmail.com>
2023-05-12 16:31:58 +0100

    move mass_bias_drift plot

--- latools/helpers/plot.py
+++ latools/helpers/plot.py
@@ -1289,36 +1289,3 @@ def stackhist(data_arrays, bins=None, bin_range=(1,99), yoffset=0, ax=None, **kw
         y0 += yn
     
     return ax
-    
-    
-def mass_bias_drift(self, analyte_ratios):
-    
-    if isinstance(analyte_ratios, str):
-        analyte_ratios = [analyte_ratios]
-
-    if analyte_ratios is None:
-        # analytes = self._calib_analytes
-        analyte_ratios = self.analytes_sorted(self._srm_id_analyte_ratios)
-        # analytes = self.analytes_sorted(self.analytes.difference([self.internal_standard]))
-
-    n = len(analyte_ratios)
-    
-    fig, axs = plt.subplots(n, 1, figsize=(10, 2*n), sharex=True)
-    if n == 1:
-        axs = [axs]
-
-    for a, ax in zip(analyte_ratios, axs):
-        # ax.scatter(self.calib_params.index, unp.nominal_values(self.calib_params[a]), color=self.cmaps[a])
-        ax.errorbar(self.calib_params.index, unp.nominal_values(self.calib_params[a]).flat, unp.std_devs(self.calib_params[a]).flat, fmt='o', color=self.cmaps[a])
-
-        ax.set_xlim(*ax.get_xlim())
-        xn = np.linspace(*ax.get_xlim(), 200)
-        
-        pred = self.calib_ps[a]['m'].new(xn)
-        
-        ax.plot(xn, unp.nominal_values(pred), color=self.cmaps[a])
-        ax.fill_between(xn, unp.nominal_values(pred) - unp.std_devs(pred), unp.nominal_values(pred) + unp.std_devs(pred), color=self.cmaps[a], alpha=0.2)
-
-        ax.set_ylabel(a)
-    
-    axs[-1].set_xlabel('Time (s)')
