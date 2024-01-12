0fb641067be46ca144ad4336fb858e3b51fbd932
Oscar Branson <ob266@cam.ac.uk>
2024-01-12 07:30:40 +0000

    complete move to VERSION instead of __version__

--- latools/__init__.py
+++ latools/__init__.py
@@ -17,8 +17,7 @@ from .helpers import config
 from .helpers import chemistry
 from . import preprocessing
 
-__version__ = '0.3.30'
-VERSION = __version__
+VERSION = '0.3.30'
 
 def cite(output='text'):
     """
