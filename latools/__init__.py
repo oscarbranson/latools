e2197bb650ae952cab4f88bf98252a01dd6d213c
Oscar Branson <ob266@cam.ac.uk>
2024-01-12 07:23:09 +0000

    moved to VERSION to work with PyPi schema

--- latools/__init__.py
+++ latools/__init__.py
@@ -17,7 +17,7 @@ from .helpers import config
 from .helpers import chemistry
 from . import preprocessing
 
-__version__ = '0.3.30-dev'
+__version__ = '0.3.30'
 VERSION = __version__
 
 def cite(output='text'):
