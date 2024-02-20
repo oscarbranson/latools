ad0ab6813f0aee8874c32bd6770bd218ec16684f
Oscar Branson <oscarbranson@gmail.com>
2024-02-20 14:27:13 +0000

    modifications to work with multi-part vertices

--- latools/mapping/io.py
+++ latools/mapping/io.py
@@ -103,32 +103,46 @@ def load_traces(dir, scans, exclude='NIST'):
     for f in fs:
         name = os.path.basename(f).replace('_calibrated.csv', '')
         t = load_trace(f)
-        x, y, d = time_to_xyd(t, get_scan_by_description(name, scans))
-        t['x'] = x
-        t['y'] = y
-        t['distance'] = d
-        
-        out[name] = t
+        scan = get_scan_by_description(name, scans)
+        if scan is not None:
+            x, y, d = time_to_xyd(t, scan)
+            t['x'] = x
+            t['y'] = y
+            t['distance'] = d
+            
+            out[name] = t
     return out
 
 def  time_to_xyd(traces, scaninfo, start_offset_um=None):
-    # TODO: only works for 2 vertices.
     x, y, _ = scaninfo['VertexList'].T
 
     dx = np.diff(x)
     dy = np.diff(y)
 
-    vertex_distance_um = (dx**2 + dy**2)**0.5
-    trace_distance_um = 1e2 * traces.index.values / float(scaninfo['AblationSettings']['ScanSpeed'])
+    trace_x = np.zeros(len(traces))
+    trace_y = np.zeros(len(traces))
+
+    total_trace_distance_um = traces.index.values * float(scaninfo['AblationSettings']['ScanSpeed'])
+    total_vertex_distance_um = np.sum((dx**2 + dy**2)**0.5)
 
     if start_offset_um is None:
-        delta_difference_um = vertex_distance_um.max() - trace_distance_um.max()
+        delta_difference_um = total_vertex_distance_um.max() - total_trace_distance_um.max()
         start_offset_um = delta_difference_um / 2
 
-    trace_distance_offset_um = trace_distance_um + start_offset_um
+    total_trace_distance_offset_um = total_trace_distance_um + start_offset_um
 
-    f_distance = trace_distance_offset_um / vertex_distance_um
-    trace_x = x[0] + dx * f_distance
-    trace_y = y[0] + dy * f_distance
+    current_distance_um = 0
 
-    return trace_x, trace_y, trace_distance_offset_um
+    for idx, idy in zip(dx, dy):
+
+        vertex_distance_um = current_distance_um + (idx**2 + idy**2)**0.5
+
+        ind = (total_trace_distance_offset_um > current_distance_um) & (total_trace_distance_offset_um <= vertex_distance_um)
+        
+        f_distance = (total_trace_distance_offset_um[ind] - current_distance_um) / vertex_distance_um
+        trace_x[ind] = x[0] + idx * f_distance
+        trace_y[ind] = y[0] + idy * f_distance
+        
+        current_distance_um += vertex_distance_um
+
+    return trace_x, trace_y, total_trace_distance_offset_um
